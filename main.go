36bb40ae8967707026e590c1f12b3c6451a45901
Alexander Kevin <41278885+aalexanderkevin@users.noreply.github.com>
2021-01-18 23:17:46 +0700

    move api routes code to main.go

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	_ "tap-talk/routes"
+	"log"
+	"os"
+	"tap-talk/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Initialize Router Server
+	server := gin.Default()
+
+	// Router Handlers
+	server.POST("/register", controllers.InsertUser)
+	server.POST("/login", controllers.Login)
+	server.GET("/diary/:year/:quarter", controllers.GetDiary)
+	server.POST("/diary", controllers.CreateDiary)
+	server.DELETE("/logout", controllers.Logout)
+
+	log.Fatal(server.Run(":" + os.Getenv("PORT")))
 
 }
